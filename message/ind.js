48c27be7e3021b48a7e8f934e4dbf87e047b2d62
Juan <kamulagi456456@gmail.com>
2021-06-29 06:01:22 +0700

    a

---
+++ message/ind.js
@@ -0,0 +1,35 @@
+exports.iklan = () => {
+     return`🔰 -----[ 「 *IKLAN ${botname}* 」 ]----- 🔰
+──────────────────────────────
+◯ *DAFTAR SEWA & BUAT BOT :*
+◯ *SEWA : 25K/GRUP (BULAN)*
+◯ *BUAT : 100K (BISA JADI OWNER)*
+◯ *PEMBAYARAN BISA MELALUI :*
+◯ *OVO, GOPAY, DANA, PULSA+10K*
+──────────────────────────────
+◯ *KEUNTUNGAN SEWA BOT :*
+◯ *1. BISA MEMASUKAN BOT KE GROUP*
+◯ *2. BISA MENGGUNAKAN FITUR PREMIUM*
+◯ *KEUNTUNGAN BUAT BOT :*
+◯ *1. BISA MENJADI OWNER BOT SENDIRI*
+◯ *2. BISA MENGGANTI NAMA BOT SENDIRI*
+◯ *3. BISA MEMBAWA BOT KE GROUP*
+◯ *4. BISA MENGGUNAKAN COMMAND OWNER*
+◯ *5. BISA MENYEWAKAN BOT KEMBALI*
+──────────────────────────────
+◯ *JIKA MINAT IKLAN DIATAS*
+◯ *HARAP HUBUNGI NOMOR DIBAWAH :*
+◯ *wa.me/${owner}*
+──────────────────────────────
+🔰 -----[「 *POWERED BY ${ownername}* 」]----- 🔰`
+}
+
+exports.donasi = () => {
+     return`*DONASI KAK*
+Pulsa : 0895-1004-8004
+Gopay : 0896-0371-2083
+Dana : 0896-0371-2083
+Saweria : https://saweria.co/rahayu
+
+Donasi supaya bot terus update & Aktif`
+}
